dffc7e7a7fcbbae8fcde189b0b150e11bc695c1c
shabdendhu.tripathy <shabdendhus@lecturenotes.in>
2024-05-20 03:20:58 +0530

    compare

--- app/(app)/(pages)/compare/components/Table/index.tsx
+++ app/(app)/(pages)/compare/components/Table/index.tsx
@@ -1,8 +1,12 @@
-import classNames from 'classnames';
-import Badge from '../../../university-detail/sub-components/badge';
-import UniversityBadge from '../UniversityBadge';
-import _ from 'lodash';
-import './index.css';
+//@ts-nocheck
+"use client";
+import classNames from "classnames";
+import Badge from "../../../university-detail/sub-components/badge";
+import UniversityBadge from "../UniversityBadge";
+import _, { set } from "lodash";
+import "./index.css";
+import { useEffect, useState } from "react";
+import { fetchData } from "@/app/(app)/services/institution";
 
 type Criteria = {
   coreCriteria: string[];
@@ -11,116 +15,229 @@ type Criteria = {
   [key: string]: string[]; // Add this index signature
 };
 
-const universities = [
+const defaultunivercity = [
   {
-    name: 'CMR University',
+    name: "CMR University",
     coreCriteria: [
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond'
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
     ],
-    advancedCriteria: ['Diamond', 'Diamond', 'Diamond', 'Diamond', 'Diamond'],
+    advancedCriteria: ["Diamond", "Diamond", "Diamond", "Diamond", "Diamond"],
     otherFactors: [
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond'
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
     ],
-    visitSite: 'Visit Site'
+    visitSite: "Visit Site",
   },
   {
-    name: 'Vignan University',
+    name: "Vignan University",
     coreCriteria: [
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond'
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
     ],
-    advancedCriteria: ['Diamond', 'Diamond', 'Diamond', 'Diamond', 'Diamond'],
+    advancedCriteria: ["Diamond", "Diamond", "Diamond", "Diamond", "Diamond"],
     otherFactors: [
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond'
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
     ],
-    visitSite: 'Visit Site'
+    visitSite: "Visit Site",
   },
   {
-    name: 'Vignan University',
+    name: "Vignan University",
     coreCriteria: [
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond'
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
     ],
-    advancedCriteria: ['Diamond', 'Diamond', 'Diamond', 'Diamond', 'Diamond'],
+    advancedCriteria: ["Diamond", "Diamond", "Diamond", "Diamond", "Diamond"],
     otherFactors: [
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond',
-      'Diamond'
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
+      "Diamond",
     ],
-    visitSite: 'Visit Site'
-  }
+    visitSite: "Visit Site",
+  },
 ];
 
-const criteria: Criteria = {
-  coreCriteria: [
-    'Employability',
-    'Teaching & Learning',
-    'Faculty Quality',
-    'Student Diversity',
-    'Facilities',
-    'Social Responsibility',
-    'Governance & Structure',
-    'Diversity & Accessibility'
-  ],
-  advancedCriteria: [
-    'Arts and Culture',
-    'Faculty Diversity',
-    'Entrepreneurship',
-    'Research',
-    'Innovation'
-  ],
-  otherFactors: [
-    'Internationalization',
-    'Academic Development',
-    'Location',
-    'Size',
-    'Year of Establishment',
-    'Status'
-  ]
-};
+// const criteria: Criteria = {
+//   coreCriteria: [
+//     "Employability",
+//     "Teaching & Learning",
+//     "Faculty Quality",
+//     "Student Diversity",
+//     "Facilities",
+//     "Social Responsibility",
+//     "Governance & Structure",
+//     "Diversity & Accessibility",
+//   ],
+//   advancedCriteria: [
+//     "Arts and Culture",
+//     "Faculty Diversity",
+//     "Entrepreneurship",
+//     "Research",
+//     "Innovation",
+//   ],
+//   otherFactors: [
+//     "Internationalization",
+//     "Academic Development",
+//     "Location",
+//     "Size",
+//     "Year of Establishment",
+//     "Status",
+//   ],
+// };
 
 // @ts-ignore
 
 export default function CompareInstitutions() {
+  const [universities, setUniversities] = useState([]);
+  const [selectedUniversities, setSelectedUniversities] = useState({
+    type: "universities",
+    ids: [1, 2, 3],
+  });
+  const [criteria, setCriteria] = useState({
+    coreCriteria: [],
+    advancedCriteria: [],
+    otherFactors: [
+      "Internationalization",
+      "Academic Development",
+      "Location",
+      "Size",
+      "Year of Establishment",
+      "Status",
+    ],
+  });
+
+  const getUnivercities = () => {
+    // Iterate over selected universities
+    selectedUniversities.ids.map((e, i) => {
+      // Fetch data for each university
+      fetchData(selectedUniversities.type + "/" + e, { depth: 3 }).then(
+        (ele) => {
+          // Extract category ratings
+          const categoryRatings = ele.ratings["category-ratings"];
+          console.log({ categoryRatings });
+          // Create a mapping of badge names to URLs
+          const badgeMapping = {};
+          categoryRatings.forEach((rating) => {
+            badgeMapping[rating.category_rating.badges_name] =
+              rating.category_rating.badges_image.url;
+          });
+
+          // Create the coreCriteria array
+          const coreCriteria = criteria.coreCriteria.map(
+            (category) => badgeMapping[category] || "NA"
+          );
+
+          // check if ele.institution_name available in universities
+          // if not available then push ele.institution_name in universities
+          // else skip
+          setUniversities((prevUniversities) => {
+            if (!prevUniversities.some((uni) => uni.id === ele.id)) {
+              // If not, add the university with its coreCriteria
+              return [
+                ...prevUniversities,
+                {
+                  id: ele.id,
+                  name: ele.institution_name,
+                  coreCriteria,
+                },
+              ];
+            }
+            // If already present, return the previous state
+            return prevUniversities;
+          });
+
+          console.log({ coreCriteria });
+        }
+      );
+    });
+  };
+
+  useEffect(() => {
+    fetchData("university-category-rating", {
+      page: 0,
+      limit: 100,
+      depth: 1,
+    }).then((e) =>
+      setCriteria((state) => ({
+        ...state,
+        coreCriteria: e.docs.map((i: any) => i.badges_name),
+      }))
+    );
+    fetchData("university-subject-ratings", {
+      page: 0,
+      limit: 100,
+      depth: 1,
+    }).then((e) =>
+      setCriteria((state) => ({
+        ...state,
+        advancedCriteria: e.docs.map((i: any) => i.badges_name),
+      }))
+    );
+  }, []);
+  useEffect(() => {
+    if (criteria.coreCriteria.length) {
+      getUnivercities();
+    }
+  }, [criteria]);
+
+  useEffect(() => {
+    console.log(universities);
+  }, [universities]);
+
   return (
     <div className="overflow-x-auto border-[1px] border-[#EAEAEA] w-full text-[16px]">
       <table className="min-w-full divide-y divide-gray-200 ">
         <thead>
           <tr>
             <th></th>
-            <th scope="col">
+            {universities.map((e) => (
+              <th scope="col">
+                <div className="flex flex-col items-center justify-center py-0 pr-[7px] pl-0 box-border gap-[0.4px]">
+                  <div className="self-stretch flex flex-row items-center justify-center py-0 pr-[7px] pl-1">
+                    <img
+                      className="h-[164.6px] flex-1 relative max-w-full overflow-hidden object-cover z-[2]"
+                      loading="lazy"
+                      alt=""
+                      src="/image-20@2x.png"
+                    />
+                  </div>
+                  <h3 className="m-0 relative text-inherit font-bold font-inherit z-[2] mq450:text-lgi">
+                    {e.name}
+                  </h3>
+                </div>
+              </th>
+            ))}
+            {/* <th scope="col">
               <div className="flex flex-col items-center justify-center py-0 pr-[7px] pl-0 box-border gap-[0.4px]">
                 <div className="self-stretch flex flex-row items-center justify-center py-0 pr-[7px] pl-1">
                   <img
@@ -149,30 +266,13 @@ export default function CompareInstitutions() {
                   CMR University
                 </h3>
               </div>
-            </th>
-            <th scope="col">
-              {' '}
-              <div className="flex flex-col items-center justify-center py-0 pr-[7px] pl-0 box-border gap-[0.4px]">
-                <div className="self-stretch flex flex-row items-center justify-center py-0 pr-[7px] pl-1">
-                  <img
-                    className="h-[164.6px] flex-1 relative max-w-full overflow-hidden object-cover z-[2]"
-                    loading="lazy"
-                    alt=""
-                    src="/image-20@2x.png"
-                  />
-                </div>
-                <h3 className="m-0 relative text-inherit font-bold font-inherit z-[2] mq450:text-lgi">
-                  CMR University
-                </h3>
-              </div>
-            </th>
+            </th> */}
           </tr>
           <tr>
             <th className="px-6 py-2 text-left text-xs tracking-wider  text-[16px] font-bold mq450:text-lgi align-bottom">
               {`Core Criteria (Badges)`}
             </th>
             <th scope="col">
-              {' '}
               <div className="flex flex-col items-center">
                 <img
                   className="w-[152px] h-[108px] object-cover z-[1]"
@@ -183,7 +283,6 @@ export default function CompareInstitutions() {
               </div>
             </th>
             <th scope="col">
-              {' '}
               <div className="flex flex-col items-center">
                 <img
                   className="w-[152px] h-[108px] object-cover z-[1]"
@@ -194,7 +293,6 @@ export default function CompareInstitutions() {
               </div>
             </th>
             <th scope="col">
-              {' '}
               <div className="flex flex-col items-center">
                 <img
                   className="h-[108px] object-cover z-[1]"
@@ -209,19 +307,21 @@ export default function CompareInstitutions() {
         {Object.keys(criteria).map((section, sectionIndex) => (
           <tbody
             key={sectionIndex}
-            className="bg-white divide-y divide-gray-200">
+            className="bg-white divide-y divide-gray-200"
+          >
             {sectionIndex === 0 || (
               <tr>
                 <td colSpan={universities.length + 1} className="h-[77px]"></td>
               </tr>
             )}
-            {section !== 'coreCriteria' && (
+            {section !== "coreCriteria" && (
               <tr className="">
                 <td
                   colSpan={universities.length + 1}
-                  className="px-6 py-2 text-left text-xs tracking-wider  text-[16px] font-bold mq450:text-lgi h-[50px]">
+                  className="px-6 py-2 text-left text-xs tracking-wider  text-[16px] font-bold mq450:text-lgi h-[50px]"
+                >
                   {_.capitalize(
-                    `${section.replace(/([A-Z])/g, ' $1').trim()} (Badges)`
+                    `${section.replace(/([A-Z])/g, " $1").trim()} (Badges)`
                   )}
                 </td>
               </tr>
@@ -232,30 +332,33 @@ export default function CompareInstitutions() {
                 <tr
                   key={criteriaIndex}
                   className={classNames({
-                    'bg-[#F8F8F8]': criteriaIndex % 2 == 0
-                  })}>
+                    "bg-[#F8F8F8]": criteriaIndex % 2 == 0,
+                  })}
+                >
                   <td className="px-6 py-3 whitespace-nowrap  font-medium text-gray-900 ">
                     {criteria}
                   </td>
-                  {universities.map((university, uniIndex) => {
-                    return (
-                      <td
-                        key={uniIndex}
-                        className="px-6 py-3 whitespace-nowrap  text-gray-500">
-                        <div className="university-badge">
-                          <UniversityBadge
-                            type={
-                              ['Diamond', 'Gold', 'Platinum'][Math.random()]
-                            }
+                  {universities.map((university, uniIndex) => (
+                    <td
+                      key={uniIndex}
+                      className="px-6 py-3 whitespace-nowrap  text-gray-500"
+                    >
+                      <div className="university-badge">
+                        {university.coreCriteria[criteriaIndex] === "NA" ? (
+                          "NA"
+                        ) : (
+                          <img
+                            src={university.coreCriteria[criteriaIndex]}
+                            alt={criteria}
                           />
-                        </div>
-                      </td>
-                    );
-                  })}
+                        )}
+                      </div>
+                    </td>
+                  ))}
                 </tr>
               )
             )}
-            {section === 'otherFactors' && (
+            {section === "otherFactors" && (
               <tr>
                 <td className="px-6 py-3 whitespace-nowrap  font-medium text-gray-900 mx-[20px] w-full">
                   Visit Site
