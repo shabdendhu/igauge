5b9a150517b3e95eb7a5c2713c36c75f6dfd678d
shabdendhu.tripathy <shabdendhus@lecturenotes.in>
2024-05-23 19:10:14 +0530

    header

--- app/(app)/components/v1/Header/Search/index.js
+++ app/(app)/components/v1/Header/Search/index.js
@@ -1,22 +1,53 @@
 "use client";
-import React from "react";
+import React, { useState, useCallback } from "react";
 import SearchIcon from "@mui/icons-material/Search";
-import { IconButton, Input, Menu, MenuItem, TextField } from "@mui/material";
+import { IconButton, Menu, TextField, Autocomplete } from "@mui/material";
+import { debounce } from "lodash";
+import { fetchData } from "@/app/(app)/services/institution";
+
 const Search = () => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [institutions, setInstitutions] = useState([]);
   const open = Boolean(anchorEl);
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
+
   const handleClose = () => {
     setAnchorEl(null);
   };
+
+  const handleSearch = useCallback(
+    debounce(async (query) => {
+      const collections = ["colleges", "schools", "universities"];
+      const searchResults = await Promise.all(
+        collections.map((collection) =>
+          fetchData(collection, {
+            filter: { institution_name: { contains: query } },
+          })
+        )
+      );
+      const allResults = searchResults.reduce(
+        (acc, current) => acc.concat(current.docs),
+        []
+      );
+      console.log(allResults);
+      setInstitutions(allResults);
+    }, 300),
+    []
+  );
+
+  const handleChange = (event, value) => {
+    setSearchQuery(value);
+    handleSearch(value);
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center w-5 h-5 ">
-      {/* <img className="w-5 h-5 " alt="" src="/search.svg" /> */}
+    <div className="flex flex-col items-center justify-center w-5 h-5">
       <IconButton
-        onClick={handleClick} // Triggered when the mouse enters the component
-        // onBlur={handleClose}
+        onClick={handleClick}
         size="small"
         sx={{ m: 0, p: 0, display: "flex", alignItems: "center" }}
         aria-controls={open ? "account-menu" : undefined}
@@ -35,9 +66,9 @@ const Search = () => {
           elevation: 0,
           sx: {
             overflow: "visible",
-            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            border: "2px solid orange",
             borderRadius: 0,
-            width: 200,
+            width: 300,
             mt: 1.5,
             px: 0,
             py: 0,
@@ -49,7 +80,25 @@ const Search = () => {
         transformOrigin={{ horizontal: "right", vertical: "top" }}
         anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
-        <TextField className="w-full h-full" />
+        <Autocomplete
+          id="input-with-icon-adornment"
+          freeSolo
+          options={institutions}
+          getOptionLabel={(r) => r.institution_name}
+          value={searchQuery}
+          onChange={handleChange}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              placeholder="Search for institution"
+              InputProps={{
+                ...params.InputProps,
+                startAdornment: <SearchIcon sx={{ mr: 1 }} />,
+                className: "w-full h-full px-5 py-3",
+              }}
+            />
+          )}
+        />
       </Menu>
     </div>
   );
